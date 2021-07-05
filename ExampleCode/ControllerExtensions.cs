65d2d64b936be9a58c812d5906b192d5a271135b
Zheorgyan <52031840+Zheorgyan@users.noreply.github.com>
2021-07-05 16:25:25 +0300

    Add files via upload

---
+++ ExampleCode/ControllerExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Mimic_Panel_Sklad3.Com
+{
+    public static class ControllerExtensions
+    {
+        //http://henkmollema.blogspot.ru/2013/10/render-partial-view-to-string.html
+        /// <summary>
+        /// Renders the specified partial view to a string.
+        /// </summary>
+        /// <param name="controller">The current controller instance.</param>
+        /// <param name="viewName">The name of the partial view.</param>
+        /// <param name="model">The model.</param>
+        /// <returns>The partial view as a string.</returns>
+        public static string RenderPartialViewToString(this Controller controller, string viewName, object model)
+        {
+            if (string.IsNullOrEmpty(viewName))
+            {
+                viewName = controller.ControllerContext.RouteData.GetRequiredString("action");
+            }
+
+            controller.ViewData.Model = model;
+
+            using (var sw = new StringWriter())
+            {
+                // Find the partial view by its name and the current controller context.
+                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(controller.ControllerContext, viewName);
+
+                // Create a view context.
+                var viewContext = new ViewContext(controller.ControllerContext, viewResult.View, controller.ViewData, controller.TempData, sw);
+
+                // Render the view using the StringWriter object.
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
