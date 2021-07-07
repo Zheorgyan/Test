6cd54951e5ac2357b212ca2293bd06e24911dc98
Zheorgyan <52031840+Zheorgyan@users.noreply.github.com>
2021-07-07 13:37:32 +0300

    MVC-доработка

---
+++ ExampleCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
